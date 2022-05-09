fbe00c1e451233cdbd5f9dfb5e37fb4429a00bc4
Stuperfy <90753343+Stuperfy@users.noreply.github.com>
2022-05-09 13:58:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    video= createCapture(VIDEO)
+    video.size(550, 500)
+
+    canvas= createCanvas(550, 550);
+    canvas.position(560, 150)
+
+    poseNet= ml5.poseNet(video, modelloaded)
+    poseNet.on('pose', gotPoses)
+}
+function modelloaded(){
+    console.log("Model is loaded")
+}
+function gotPoses(result){
+    if(result.length>0){
+        console.log(result)
+    }
+    
+}
